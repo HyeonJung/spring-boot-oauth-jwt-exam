a7e52c41e0e60353cbbb257e5f2bd5f34f2e6920
윤현중 <guswnd1592@gaeasoft.co.kr>
2017-10-11 14:37:14 +0900

    최초 커밋

---
+++ src/main/java/com/example/oauthjwtexam/OauthJwtExamApplication.java
@@ -0,0 +1,12 @@
+package com.example.oauthjwtexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthJwtExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthJwtExamApplication.class, args);
+	}
+}
